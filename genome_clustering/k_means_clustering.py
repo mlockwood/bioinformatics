61358b090ec85fcd04e45b60edce5328babc4c2b
Michael Lockwood <lockwm@uw.edu>
2017-06-11 22:21:00 -0700

    Finished lloyd algorithm.

--- genome_clustering/k_means_clustering.py
+++ genome_clustering/k_means_clustering.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import copy
+from numpy.random import choice
 import sys
 
 
@@ -28,9 +29,9 @@ class KMeansCluster(object):
         :param lines: string delimited values
         :return: set of data points
         """
-        data = set()
+        data = []
         for line in lines:
-            data.add(tuple(float(f) for f in line.rstrip().split()))
+            data.append(tuple(float(f) for f in line.rstrip().split()))
         return data
 
     @staticmethod
@@ -57,7 +58,6 @@ class KMeansCluster(object):
         :param points: points that form a cluster
         :return: the point/center of gravity
         """
-        print(points)
         # First sort point information by dimension
         gravity = dict((i, []) for i in range(len(points[0])))
         for point in points:
@@ -71,12 +71,12 @@ class KMeansCluster(object):
         for dimension in sorted(gravity.keys()):
             point.append(sum(gravity[dimension]) / len(gravity[dimension]))
 
-        print(point)
         return tuple(point)
 
     def farthest_first_traversal(self):
         """
-        Initialize the centers for the k means clustering function.
+        Initialize the centers for a k means clustering function using
+        farthest points.
         :return: set of centers
         """
         last_center = next(iter(self.centers))
@@ -97,7 +97,34 @@ class KMeansCluster(object):
                     farthest = (closest, point)
 
             self.centers.add(farthest[1])
+            print(farthest[0] ** 0.5)
             last_center = farthest[1]
+
+    def k_means_initializer(self):
+        """
+        Initialize the centers for a k means clustering function using
+        probabilities.
+        :return: set of centers
+        """
+        last_center = next(iter(self.centers))
+        while len(self.centers) < self.k:
+
+            # Add a new center by weighted random probabilities
+            probs = []
+            for point in self.data:
+
+                # Store lookup of euclidean distance between point and centers
+                if point not in self.distances:
+                    self.distances[point] = {}
+                self.distances[point][last_center] = KMeansCluster.euclidean_distance(point, last_center)
+
+                # Add minimum distance as probability of being selected
+                probs.append(self.distances[point][min(self.distances[point], key=self.distances[point].get)])
+
+            probs = [x / sum(probs) for x in probs]
+            last_center = tuple(float(s) for s in choice([' '.join(str(f) for f in x) for x in list(self.data)],
+                                                         1, p=probs)[0].split())
+            self.centers.add(last_center)
 
     def squared_error_distortion(self):
         """
@@ -119,7 +146,7 @@ class KMeansCluster(object):
         return distortion / len(self.data)  # remember to return an average
 
     def lloyd_algorithm(self):
-        self.farthest_first_traversal()
+        self.k_means_initializer()
         prev_centers = None
         while prev_centers != self.centers:
             prev_centers = copy.deepcopy(self.centers)
@@ -144,7 +171,7 @@ class KMeansCluster(object):
     def print_centers(self):
         out = ''
         for center in self.centers:
-            out += ' '.join(center)
+            out += ' '.join(str(round(f, 3)) for f in center)
             out += '\n'
         return out
 
@@ -153,3 +180,11 @@ class KMeansCluster(object):
 # k = int(lines[0].split()[0])
 # data = KMeansCluster.load_points_from_strings(lines[1:])
 # print(KMeansCluster(data, k).print_centers())
+
+obj1 = KMeansCluster([(2, 8), (2, 5), (6, 9), (7, 5), (5, 2)], 3, centers={*[(3, 5), (5, 4)]})
+obj1.farthest_first_traversal()
+
+obj2 = KMeansCluster([(2, 6), (4, 9), (5, 7), (6, 5), (8, 3)], 2, centers={*[(4, 5), (7, 4)]})
+print(obj2.squared_error_distortion())
+
+print(KMeansCluster.center_of_gravity([(17, 0, -4), (3, 14, 23), (9, 7, 16), (7, 3, 5)]))
--- genome_clustering/test/test_k_means_clustering.py
+++ genome_clustering/test/test_k_means_clustering.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from genome_clustering.k_means_clustering import *
 
 
@@ -9,8 +11,18 @@ __github__ = 'mlockwood'
 __email__ = 'lockwm@uw.edu'
 
 
+skip_random = pytest.mark.skipif(
+    not pytest.config.getoption("--random"),
+    reason="need --random option to run"
+)
+
+
 def test_euclidean_distance():
     assert KMeansCluster.euclidean_distance((1, 2), (3, 2)) == 4
+
+
+def test_center_of_gravity():
+    assert KMeansCluster.center_of_gravity([(4, 3, 8), (9, 4, 2), (2, 2, 2)]) == (5, 3, 4)
 
 
 def test_squared_error_distortion():
@@ -23,8 +35,11 @@ def test_squared_error_distortion():
     assert 18.245 <= k_means_obj.squared_error_distortion() <= 18.246
 
 
+@skip_random
 def test_lloyd_algorithm():
     data_lines = ['1.3 1.1', '1.3 0.2', '0.6 2.8', '3.0 3.2', '1.2 0.7', '1.4 1.6', '1.2 1.0', '1.2 1.1', '0.6 1.5',
                   '1.8 2.6', '1.2 1.3', '1.2 1.0', '0.0 1.9']
     k_means_obj = KMeansCluster(KMeansCluster.load_points_from_strings(data_lines), 2)
-    assert (1.800, 2.867) in k_means_obj.centers and (1.060, 1.140) in k_means_obj.centers
+    for center in k_means_obj.centers:
+        center = tuple(round(f, 3) for f in center)
+        assert center in [(1.8, 2.867), (1.060, 1.140)]
